058fdb7e9f9d7a537e10ba5ae34d0b446f44c951
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-21 20:44:50 -0500

    add merge_sort method

--- merge_sort.rb
+++ merge_sort.rb
@@ -9,4 +9,23 @@ def merge_sort(arr)
 	right_arr = arr[mid..-1]
 
 	merge(left_arr, right_arr)
+	#binding.pry
 end
+
+def merge(left_arr, right_arr)
+	new_arr = []
+
+	until left.arr.epmty? && rigth_arr.empty?
+		if left.arr[0] < right_arr[0]
+
+			new_arr << left_arr[0]
+
+		elsif left.arr[0] > right_arr[0]
+
+			new_arr << right_arr[0]
+		end
+		new_arr
+		#binding.pry
+end
+
+merge_sort([1,3,7,2,5])
