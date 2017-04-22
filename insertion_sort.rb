521c862695101f1fadb3251992605f655eec0a8e
Chad Lucas <chad.lucas.19@gmail.com>
2017-04-21 20:32:31 -0500

    add insertion_sort method

--- insertion_sort.rb
+++ insertion_sort.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def insertion(array, right_index, value)
 	i = right_index
 	while (i >= 0 && array[i] > value)
@@ -6,3 +8,15 @@ def insertion(array, right_index, value)
 	end
 	array[i + 1] = value
 end
+
+def insertion_sort(arr)
+	counter = 0
+	while counter < arr.length-1
+		insertion(arr, counter, arr[counter+1])
+		counter += 1
+	end
+	arr
+	#binding.pry
+end
+
+insertation_sort([1,3,7,-2,5])
